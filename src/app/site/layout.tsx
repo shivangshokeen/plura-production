887e1db39554e63287bb090ff3c050474ee37f86
Shivang shokeen <shivangshokeen@Shivangs-Mac-mini.local>
2024-04-02 23:48:34 +0530

    moving to production

---
+++ src/app/site/layout.tsx
@@ -0,0 +1,20 @@
+import Navigation from '@/components/site/navigation'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import React from 'react'
+
+const Layout = ({children}: { children:React.ReactNode}) => {
+  return (
+    <ClerkProvider appearance={{
+      baseTheme: dark
+    }}>
+   
+    <main className='h-full'>
+      <Navigation />
+        {children}
+    </main>
+    </ClerkProvider>
+  )
+}
+
+export default Layout
